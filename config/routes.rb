7e7be89ac7d371e57733d9602ba471672f442dce
Jenee Geer <jeneegeer@gmail.com>
2018-06-11 09:47:04 -0500

    re-engineer the new view to connect the bootstrap form to the messages model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#new'
 
-  post '/', to: 'messages#create'
+  post '/', to: 'messages#create', as: :messages
   get '/:token/unlock', to: 'messages#unlock', as: :unlock
   get '/:token', to: 'messages#show', as: :display
 
