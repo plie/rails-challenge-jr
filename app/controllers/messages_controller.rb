3aa338d3ba20afef568ffe362ec67fc865491c27
Jenee Geer <jeneegeer@gmail.com>
2018-06-14 13:00:25 -0500

    add token and path info back to view, add redirecting to root when user tries to reload a previously viewed message that has been deleted.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.password != '' && @message.save
-      flash[:success] = "Your message has been saved."
+      flash[:success] = "Your message has been saved. Your token is: #{@message.token}. Use this URL to retrieve your message: /token/password"
       Rails.logger.info("Here is your token: #{@message.token}. Use this URL to retrieve: /token/password")
       redirect_to root_path
     else
@@ -18,14 +18,16 @@ class MessagesController < ApplicationController
 
   def show
     @message_temp = Message.find_by_token(params[:token])
-    if @message_temp.password == params[:password]
+    if @message_temp && @message_temp.password == params[:password]
       Rails.logger.info("Authentication succeeded")
       @message = @message_temp
       flash.now[:warning] = 'This message has been deleted. Once you leave this page this message will no longer be available.'
       @message.destroy
+    elsif @message_temp == nil
+      redirect_to root_path 
     else
       Rails.logger.info("Authentication failed")
-      flash[:danger] = 'Please use a valid password and token.'
+      flash[:danger] = "Please use a valid password and token. Your token is: #{@message_temp.token}. Use this URL to retrieve your message: /token/password"
       redirect_to root_path
     end
   end
