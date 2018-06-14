ec6884d3b577b6e4d4b6a7f4e6ac811e987429cd
Jenee Geer <jeneegeer@gmail.com>
2018-06-14 12:27:39 -0500

    fix routes, improve flash messages for better flow, remove token from view, fix password authentication, improve views: no longer lose new message if forget to add password and add link to quickly create a new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,37 +1,36 @@
 class MessagesController < ApplicationController
-  before_action :get_message, only: [:new]
 
   def new
+    @message = Message.new
   end
 
   def create
     @message = Message.new(message_params)
     if @message.password != '' && @message.save
-      flash[:success] = "Thank you, your message has been saved. Here is your URL: /#{@message.token}/YourPasswordHere."
-      p "Here is your token: #{@message.token}"
-      redirect_to messages_path
+      flash[:success] = "Your message has been saved."
+      Rails.logger.info("Here is your token: #{@message.token}. Use this URL to retrieve: /token/password")
+      redirect_to root_path
     else
-      flash[:danger] = 'Message not saved. Need to create a valid password. Please try again.'
-      redirect_to messages_path
+      flash.now[:danger] = 'Message not saved. Need to create a valid password.'
+      render action: "new"
     end
   end
 
   def show
-    message = Message.find_by_token(params[:token])
-    if message.password == params[:password]
-      @message = message
-      flash[:danger] = 'This message has been deleted. Once you leave this page this message will no longer be available.'
+    @message_temp = Message.find_by_token(params[:token])
+    if @message_temp.password == params[:password]
+      Rails.logger.info("Authentication succeeded")
+      @message = @message_temp
+      flash.now[:warning] = 'This message has been deleted. Once you leave this page this message will no longer be available.'
       @message.destroy
     else
-      flash[:danger] = 'Please use a valid password and token, using this URL format: /token/password.'
+      Rails.logger.info("Authentication failed")
+      flash[:danger] = 'Please use a valid password and token.'
+      redirect_to root_path
     end
   end
 
   private
-
-  def get_message
-    @message = Message.new
-  end
 
   def message_params
     params.require(:message).permit(:password, :message_body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#new'
 
-  post '/', to: 'messages#create', as: :messages
+  get 'messages', to: 'messages#new'
+  post 'messages', to: 'messages#create'
   get '/:token/:password', to: 'messages#show'
 end
